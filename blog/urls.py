3cd3f5faa5400a4e42aae5091a02c9d3c30b6b6c
nisargadalja <59527108+nisargadalja@users.noreply.github.com>
2020-12-03 00:27:19 +0530

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path,include
-# from . import views
-# #from django.views.generic import TemplateView
-# urlpatterns = [
-#     path('',views.index,name="blog"),
-#
-#     #path("blogPost/<int:id>", views.blogpost, name="blogHome")
-#      #path("blogPost/", TemplateView.as_view(template_name='index.html'))
-#     # it is templateView is use when we need to direct go to the html page without any logic or calculation
-# ]
-
 from django.urls import path,include
 from . import views
 
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,8 @@
-#
-# from django.http import HttpResponse
-# from django.shortcuts import render
-# from .models import BlogPost
-# # Create your views here.
-# def index(request):
-#     return render(request,'blog/index.html');
-# def blogpost(request, id):
-#     post = BlogPost.objects.filter(post_id = id)[0]
-#     print(post)
-#     return render(request, 'blog/blogPost.html',{'post':post})
+
 from django.shortcuts import render
 from math import ceil
-# Create your views here.
 from django.http import HttpResponse
 from .models import BlogPost
-
 def index(request):
     myPosts=BlogPost.objects.all();
     return render(request, 'blog/index.html',{'myPosts': myPosts})
@@ -24,5 +12,4 @@ def blogPost(request, id):
     max1=BlogPost.objects.all();
     first1=BlogPost.objects.first();
     last1=BlogPost.objects.last();
-
     return render(request, 'blog/blogPost.html',{'post':post,'prev':int(id-1),'next':int(id+1),'first':first1,'last':last1})
